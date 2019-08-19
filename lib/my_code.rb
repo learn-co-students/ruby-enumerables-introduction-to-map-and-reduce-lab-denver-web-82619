16dc9c493270faa440e51e8711b83b9dfbd6f40f
Gage Thornberry <gagedoug13@gmail.com>
2019-08-19 18:14:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,56 @@
 def map_to_negativize(source_array)
   new_array = []
-  var i = 0
-  while i < source_array.length do
-    new_array.push(source_array[i] * -1)
-    i++
+  source_array.each do |item|
+  new_array.push(item * -1)
   end
   p new_array
+end
+
+def map_to_no_change(array)
+  p array
+end
+
+def map_to_double(array)
+  new_array = []
+  array.each do |item|
+  new_array.push(item * 2)
+  end
+  p new_array
+end
+
+def map_to_square(array)
+  new_array = []
+  array.each do |item|
+  new_array.push(item ** 2)
+  end
+  p new_array
+end
+
+def reduce_to_total(array,starting_point)
+  i = 0
+  sum = 0
+  if starting_point != nil
+    i = starting_point
+  end
+  while i < array.length
+    sum += array[i]
+    i++
+  end
+  p sum
+end
+
+def reduce_to_all_true(source_array)
+  if source_array.include?(false)
+    return false
+  else
+    return true
+  end
+end
+
+def reduce_to_any_true(source_array)
+  if source_array.include?(true)
+    return true
+  else
+    return false
+  end
 end
