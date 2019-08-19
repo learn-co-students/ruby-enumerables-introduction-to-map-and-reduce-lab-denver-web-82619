46b5f81de0484661567272508cbb752c144f323f
Gage Thornberry <gagedoug13@gmail.com>
2019-08-19 00:07:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 def map_to_negativize(source_array)
   new_array = []
   var i = 0
-  while (i < source_array.length) do
+  while i < source_array.length do
     new_array.push(source_array[i] * -1)
     i++
   end
-    p new_array
+  p new_array
 end
